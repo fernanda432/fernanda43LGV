1c49333f634c306174ae1d81add73ae1b15e4e0f
fernanda43 <81189772+fernanda432@users.noreply.github.com>
2021-03-22 18:11:31 -0600

    Create init.js

---
+++ js/init.js
@@ -0,0 +1,10 @@
+firebase.initializeApp({
+  
+apiKey: "AIzaSyDfWWTs-fbtEvQqL8oISUaKtMSGJ6RqMUo",
+    authDomain: "autentificacion43lfgv-81862.firebaseapp.com",
+    projectId: "autentificacion43lfgv-81862",
+    storageBucket: "autentificacion43lfgv-81862.appspot.com",
+    messagingSenderId: "326985076542",
+    appId: "1:326985076542:web:212fa6cf09baabac81266f",
+    measurementId: "G-Y1YL3MWXWM"
+});
