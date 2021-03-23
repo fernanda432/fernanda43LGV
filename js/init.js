ba7edc8f9e18caaaca55d6a5fc351a4ba055ba7a
fernanda43 <81189772+fernanda432@users.noreply.github.com>
2021-03-22 18:22:35 -0600

    Update init.js

--- js/init.js
+++ js/init.js
@@ -1,10 +1,12 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
 firebase.initializeApp({
-  
-apiKey: "AIzaSyDfWWTs-fbtEvQqL8oISUaKtMSGJ6RqMUo",
+    apiKey: "AIzaSyDfWWTs-fbtEvQqL8oISUaKtMSGJ6RqMUo",
     authDomain: "autentificacion43lfgv-81862.firebaseapp.com",
     projectId: "autentificacion43lfgv-81862",
     storageBucket: "autentificacion43lfgv-81862.appspot.com",
     messagingSenderId: "326985076542",
     appId: "1:326985076542:web:212fa6cf09baabac81266f",
     measurementId: "G-Y1YL3MWXWM"
-});
+    });
